d80256f700217fa88c5758193b1262291d67adc3
JUN GYEWON <tonyjev93@gmail.com>
2022-01-10 15:58:45 +0900

    chore: eureka client 정보 등록

--- src/main/java/com/tonyjev/userservice/UserServiceApplication.java
+++ src/main/java/com/tonyjev/userservice/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.tonyjev.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // @EnableEurekaClient 로 해도 됨
 public class UserServiceApplication {
 
     public static void main(String[] args) {
