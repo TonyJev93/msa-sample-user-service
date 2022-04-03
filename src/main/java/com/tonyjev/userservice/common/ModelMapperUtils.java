7673179b586f4d59dfed22197c178987bd055fce
JUN GYEWON <tonyjev93@gmail.com>
2022-04-03 19:32:48 +0900

    feat: add ModelMapperUtils

---
+++ src/main/java/com/tonyjev/userservice/common/ModelMapperUtils.java
@@ -0,0 +1,18 @@
+package com.tonyjev.userservice.common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ModelMapperUtils {
+
+    private static final ModelMapper mapper = new ModelMapper();
+
+    public static Object mapStrict(Object source, Class<?> targetClass) {
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mapper.map(source, targetClass);
+    }
+
+}
